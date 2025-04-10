c376319c01b033b402e98955fa81ddcf12c1eb84
ibrohim <ibrohim.dev.uz@gmail.com>
2025-04-10 18:22:01 +0500

    .

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class BaseCreatedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Category(BaseCreatedModel):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
