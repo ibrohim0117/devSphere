90594926eb0f1630446b4ad8ec376842117ca1ff
ibrohim <ibrohim.dev.uz@gmail.com>
2025-04-11 09:56:28 +0500

    .

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,28 @@ class Category(BaseCreatedModel):
 
     def __str__(self):
         return self.name
+
+
+class Tag(BaseCreatedModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Emoji(BaseCreatedModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(BaseCreatedModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    views = models.PositiveIntegerField(default=0, editable=False)
+    imoji = models.ForeignKey(Emoji, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
+
