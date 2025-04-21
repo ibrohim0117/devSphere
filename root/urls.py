6bb98cc3603b714173923120d908d0a939e8503e
ibrohim <ibrohim.dev.uz@gmail.com>
2025-04-21 16:37:04 +0500

    .

--- root/urls.py
+++ root/urls.py
@@ -1,10 +1,12 @@
 from root import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-       path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
+   path('blog/', include('blog.urls')),
+
 ]
 
 if settings.DEBUG:
