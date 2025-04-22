eaee93526578c2562953090bcba0ecf06022f2ca
ibrohim <ibrohim.dev.uz@gmail.com>
2025-04-22 11:46:40 +0500

    .

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import IndexView
+from .views import (
+    IndexView, SingleView, ArchiveView,
+    CategoryView, ElementsView, GenericsView,
+    SearchView
+)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('single/', SingleView.as_view(), name='single'),
+    path('archive/', ArchiveView.as_view(), name='archive'),
+    path('category/', CategoryView.as_view(), name='category'),
+    path('elements/', ElementsView.as_view(), name='elements'),
+    path('generics/', GenericsView.as_view(), name='generics'),
+    path('search/', SearchView.as_view(), name='search'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,28 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class SingleView(TemplateView):
+    template_name = 'single.html'
+
+
+class ArchiveView(TemplateView):
+    template_name = 'archive.html'
+
+
+class CategoryView(TemplateView):
+    template_name = 'category.html'
+
+
+class ElementsView(TemplateView):
+    template_name = 'elements.html'
+
+
+class GenericsView(TemplateView):
+    template_name = 'generic.html'
+
+
+class SearchView(TemplateView):
+    template_name = 'search.html'
+
