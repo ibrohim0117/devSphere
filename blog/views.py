a20da57ce5cfd1c41230ca5481b903e5b5b64af8
ibrohim <ibrohim.dev.uz@gmail.com>
2025-04-24 10:30:22 +0500

    .

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView, ListView
+)
+
+from blog.models import Post
 
 
-
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
+    model = Post
+    context_object_name = 'posts'
 
 
 class SingleView(TemplateView):
