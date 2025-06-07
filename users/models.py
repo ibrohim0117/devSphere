66ed1148cd0967309c885bb004194fc3b99eafe4
ibrohim <ibrohim.dev.uz@gmail.com>
2025-06-08 00:31:43 +0500

    .

--- users/models.py
+++ users/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
 
 class User(AbstractUser):
     about = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    facebook = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    leetcode = models.URLField(blank=True, null=True)
+    telegram = models.URLField(blank=True, null=True)
+
+    is_verified = models.BooleanField(default=False)
+
+    email = models.EmailField(unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    username = None
+
+    def __str__(self):
+        return self.username
