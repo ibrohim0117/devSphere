8c7b738c0a9add41726a31ad44175d6d42d6d565
ibrohim <ibrohim.dev.uz@gmail.com>
2025-06-07 18:29:26 +0500

    add filter_manager.py

---
+++ blog/filter_manager.py
@@ -0,0 +1,48 @@
+from django.db.models import Q, Count
+from django.utils.timezone import now
+from datetime import timedelta
+
+class FilterManager:
+    def __init__(self, queryset, search=None, filter_option=None, category_option=None, tag_option=None):
+        self.queryset = queryset
+        self.search = search
+        self.filter_option = filter_option
+        self.category_option = category_option
+        self.tag_option = tag_option
+
+    def all_filters(self):
+        # Qidiruv buyicha filter
+        if self.search:
+            self.queryset = self.queryset.filter(
+                Q(title__icontains=self.search) | Q(content__icontains=self.search)
+            ).distinct()
+
+        # Kategoriya buyicha filter
+        if self.category_option:
+            self.queryset = self.queryset.filter(category__slug=self.category_option)
+
+        # Teg buyicha filter
+        if self.tag_option:
+            self.queryset = self.queryset.filter(tags__name__icontains=self.tag_option).distinct()
+
+        # Qo‘shimcha filterlar
+        if self.filter_option == 'yangilar':
+            self.queryset = self.queryset.order_by('-created_at')
+        elif self.filter_option == 'top':
+            self.queryset = self.queryset.order_by('-views')
+        elif self.filter_option == 'yonmoqda':
+            self.queryset = self.queryset.filter(views__gte=100).order_by('-views')
+        elif self.filter_option == 'emoji':
+            self.queryset = self.queryset.annotate(
+                emoji_count=Count('emoji')
+            ).filter(emoji_count__gt=0).order_by('-emoji_count')
+        elif self.filter_option == 'hafta':
+            one_week_ago = now() - timedelta(days=7)
+            self.queryset = self.queryset.filter(created_at__gte=one_week_ago)
+        elif self.filter_option == 'oy':
+            one_month_ago = now() - timedelta(days=30)
+            self.queryset = self.queryset.filter(created_at__gte=one_month_ago)
+        elif self.filter_option == 'hammasi' or self.filter_option is None:
+            pass
+
+        return self.queryset
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q, Count
 from django.utils.timezone import now, timedelta
 
 from blog.models import Post, Category, Tag
+from .filter_manager import FilterManager
 
 
 class HomeView(ListView):
@@ -16,35 +17,21 @@ class HomeView(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        query = self.request.GET.get('q')
+        search = self.request.GET.get('q')
         filter_option = self.request.GET.get('filter')
+        category_option = self.request.GET.get('category')
+        tag_option = self.request.GET.get('tag')
 
+        data = FilterManager(
+            queryset=queryset,
+            search=search,
+            filter_option=filter_option,
+            category_option=category_option,
+            tag_option=tag_option,
+        )
 
-        if query:
-            queryset = queryset.filter(
-                Q(title__icontains=query) | Q(content__icontains=query)
-            ).distinct()
+        return data.all_filters()
 
-        if filter_option == 'yangilar':
-            queryset = queryset.order_by('-created_at')
-        elif filter_option == 'top':
-            queryset = queryset.order_by('-views')
-        elif filter_option == 'yonmoqda':
-            queryset = queryset.filter(views__gte=100).order_by('-views')
-        elif filter_option == 'emoji':
-            queryset = queryset.annotate(
-                emoji_count=Count('emoji')
-            ).filter(emoji_count__gt=0).order_by('-emoji_count')
-        elif filter_option == 'hafta':
-            one_week_ago = now() - timedelta(days=7)
-            queryset = queryset.filter(created_at__gte=one_week_ago)
-        elif filter_option == 'oy':
-            one_month_ago = now() - timedelta(days=30)
-            queryset = queryset.filter(created_at__gte=one_month_ago)
-        elif filter_option == 'hammasi':
-            pass
-
-        return queryset
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
